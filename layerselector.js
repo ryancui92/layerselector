7da4372ebd49edf9f97247c82ce017b43c5b06dc
ryancui <ryancuizw@qq.com>
2015-03-02 13:21:20 +0800

    1. When item's content is longer than the label width, expand it to multiple column. 2. Add an OK button in multiple mode.

--- layerselector.js
+++ layerselector.js
@@ -1,5 +1,6 @@
 /** 
- * 层级选择器-插件
+ * Layer selector version 1.0
+ * @author RyanCui
  */
 
 (function($) {
@@ -13,17 +14,17 @@
 		}
 
 		that.layers = that.config.layers;
+		that.layerCount = that.layers.length;	// How many layers
 		init.call(that);
 	};
 
 	function init() {
 		var that = this;
-		that.initMain();			// 缓存数据
-		that.bindEvent();			// 绑定事件
-		// 显示第一层数据
+		that.initMain();
+		that.bindEvent();
+		// Show the data of top layer
 		that.layerData[that.layers[0].field] = that.layers[0].dataProvider();
 		that._addTab(0);
-		// 隐藏dialog
 		that._hide();
 	}
 
@@ -58,13 +59,18 @@
 				$(this).removeClass('calendar-nav-hover');
 			});
 
+			// Multiple OK button click event
+			that.$dialog.on('click', '.ok-button', function() {
+				that._hide();
+			});
+
 			that.$tabs = $("#tabs");
 			that.$tabs.tabs({
 				border: false,
 				width: that.config.width+10,
 				height: that.config.height+10,
 				onUpdate: function(title, layer) {
-					if (that.config.multiple && layer==that.layers.length-1) {
+					if (that.config.multiple && layer==that.layerCount-1) {
 						var thisTab = that.$tabs.tabs('getTab', layer);
 						var $allLink = thisTab.panel('panel').find('.link');
 						$allLink.each(function(i, v) {
@@ -84,7 +90,7 @@
 					if (that.layers[layer].otherItem(layer==0 ? null : that.selected[that.layers[layer-1].field])) {
 						that.$otherText = $("[name='othertext"+layer+"']");
 						that.$otherText.textbox({
-							buttonText: '确定',
+							buttonText: 'OK',
 							onClickButton: function() {
 								var val = that.$otherText.textbox('getText'), data = {};
 								data[that.layers[layer].labelField] = val;
@@ -103,35 +109,35 @@
 				}
 			});
 
-			// 单击除了选择器面板外的地方隐藏面板
+			// Hide panel when click outside the panel
 			$('html').on('mousedown', function(event) {
-				// TODO 点击textbox和icon本身时出现闪烁，这里的mousedown事件没有处理
-				// 到点击textbox和icon的事件
+				// TODO When click the textbox or textbox-icon, the panel will flash,
+				// 		we should handle the event later
 				if ($(event.target).closest('#layerSelector').length==0) {
 					that._hide();
 				}
 			});
 
-			that.columnWidth = Math.floor(that.config.width/that.config.column) - 5;	// 每个 item 需要占的width
-			that.layerData = {};		// 每一层的数据
-			that.selected = {};			// 每一层的选择
+			// The minimal width of one item
+			that.columnWidth = Math.floor(that.config.width/that.config.column) - 5;
+			that.layerData = {};		// Data in each layer
+			that.selected = {};			// Selected Data in each layer
 		},
 		bindEvent: function() {
 			var that = this;
 
-			// item选择事件
 			that.$dialog.on('click', '.link', function() {
 				var $this = $(this),
-					id = $(this).data('id'),						// 选中item的id
+					id = $(this).data('id'),
 					tab = that.$tabs.tabs('getSelected'),			
-					layer = that.$tabs.tabs('getTabIndex', tab),	// 当前所在层
+					layer = that.$tabs.tabs('getTabIndex', tab),
 					data = null;
 				
 				if (!id || id=='other') {
 					return;
 				}
 				
-				// 找到对应的data
+				// Find data according to id
 				for (var i in that.layerData[that.layers[layer].field]) {
 					if ( that.layerData[that.layers[layer].field][i][that.layers[layer].idField] == id ) {
 						data = that.layerData[that.layers[layer].field][i];
@@ -139,16 +145,14 @@
 					}
 				}
 				
-				// 多选而且选择的item是最后一层
-				if (that.config.multiple && layer==that.layers.length-1) {
+				if (that.config.multiple && layer==that.layerCount-1) {
 					var lastSelected = that.selected[that.layers[layer].field], origin = false; 
-					
 					if (!lastSelected) {
 						that.selected[that.layers[layer].field] = [];
 						lastSelected = [];
 					}
 
-					// 之前找到就移除掉
+					// If already select before, then remove it
 					for (var i=0; i<lastSelected.length; i++) {
 						if (lastSelected[i][that.config.layers[layer].idField] == id) {
 							origin = true;
@@ -156,14 +160,14 @@
 							break;
 						}
 					}
-
-					// 找不到就加进选中
+					// If not select, add it in selected data
 					if (!origin) {
 						that.selected[that.layers[layer].field].push(data);
 					}
 
 					that._onSelectLayer(layer, data);
 
+					// Change CSS
 					if (origin) {
 						$this.removeClass('kkkk');
 					} else {
@@ -173,12 +177,11 @@
 					that.selected[that.layers[layer].field] = data;
 					that._onSelectLayer(layer, data);
 				}
-				// console.log(that.selected);
 			});
 		},
 		_show: function() {
 			var that = this, pos = that.$element.textbox('textbox').offset();
-			// The exact constants here need to be adjusted due to different circumstances.
+			// The exact constants here need to be adjusted due to different circumstances
 			that.$dialog.css({
 				'top': pos.top - 7 + that.$element.textbox('options').height,
 				'left': pos.left - 9
@@ -189,8 +192,10 @@
 			var that = this;
 			that.$dialog.css({'display': 'none'});
 		},
+		/** 
+		 *	Add a tab in layer
+		 */
 		_addTab: function(layer) {
-			// 添加第layer层的tab页面并跳到该页面
 			var that = this;
 			that.$tabs.tabs('add', {
 				title: that.layers[layer].prompt,
@@ -198,14 +203,31 @@
 				selected: true
 			});
 		},
+		/**
+		 * Generate the HTML code in layer
+		 * @param {number} layer The layer number begin with 0
+		 * @return {string} HTML code
+		 */
 		_generateContent: function(layer) {
-			// 生成某一层的UI
 			var that = this, obj = that.layerData[that.layers[layer].field], lineArr = [], line = '', ret = '';
-			for (var i=0; i<obj.length; i++) {
-				line += '<div style="width:' + that.columnWidth + 'px;"><label data-id="' + 
+			for (var i=0, j=0; i<obj.length; i++) {
+				var colspan = 1;
+				while (obj[i][that.layers[layer].labelField].length * that.config.chracterPixels 
+					> that.columnWidth * colspan) {
+					colspan++;
+					j++;
+				}
+				// The rest of the line cannot afford enough space for colspan
+				// Then go to next line
+				if (colspan+j >= that.config.column) {
+					lineArr.push(line);
+					line = '';
+					j = colspan-1;
+				}
+				line += '<div style="width:' + that.columnWidth*colspan + 'px;"><label data-id="' + 
 					obj[i][that.layers[layer].idField] + '" class="link">' + 
 					obj[i][that.layers[layer].labelField] + '</label></div>';
-				if (i%that.config.column == that.config.column-1) {
+				if (j%that.config.column == that.config.column-1) {
 					lineArr.push(line);
 					line = '';
 				}
@@ -216,7 +238,8 @@
 			ret = '<div style="width:'+(that.config.width)+'px;" class="line">' + 
 				lineArr.join('</div><div style="width:'+(that.config.width)+'px;" class="line">') + '</div>';
 			
-			// 添加其他选项
+			// Other item exists?
+			// TODO Change Chinese into Variable defined
 			if (that.layers[layer].otherItem(layer==0 ? null : that.selected[that.layers[layer-1].field])) {
 				var str = '<div style="width:'+(that.config.width)+'px;" class="line"><div style="width:' + 
 					that.columnWidth +'px;"><label data-id="other" class="link">其他</label></div>' +
@@ -225,27 +248,30 @@
 					'</div>';
 				ret += str;
 			}
+
+			// Add a OK button when in multiple mode
+			if (that.config.multiple && layer==that.layerCount-1) {
+				var str = '<div style="width:'+(that.config.width)+'px;text-align:center;">' +
+					'<a href="#" class="easyui-linkbutton ok-button" style="width:'+ that.columnWidth + 'px;">确定</a></div>' + 
+					'</div>';
+				ret += str;
+			}
 			return ret;
 		},
 		_onSelectLayer: function(layer, data) {
 			var that = this, newOption = false;
 			
-			// 清空layer+!层之后的选中数据
-			if (layer != that.layers.length-1) {
-				for (var i=layer+1; i<that.layers.length; i++) {
-					that.selected[that.layers[i].field] = null;
-				}
+			// Clear selected data after layer+1
+			for (var i=layer+1; i<that.layerCount; i++) {
+				that.selected[that.layers[i].field] = null;
 			}
 
-			// 将当前tab的标题改为选中的item
-			var thisTab = that.$tabs.tabs('getTab', layer);
+			// Change tab title to current selected
+			var thisTab = that.$tabs.tabs('getTab', layer), newTitle = '';
 			if (thisTab.panel('options').title != data[that.layers[layer].labelField]) {
 				newOption = true;
 			}
-			
-			// 更新当前层的标题
-			var newTitle = '';
-			if (layer==that.layers.length-1 && that.config.multiple) {
+			if (that.config.multiple && layer==that.layerCount-1) {
 				var newTitleArr = [];
 				$.each(that.selected[that.layers[layer].field], function(i, v) {
 					newTitleArr.push(v[that.layers[layer].labelField]);
@@ -261,8 +287,7 @@
 				}
 			});
 
-			// 已经选择了最后一层
-			if (layer == that.layers.length-1) {
+			if (layer == that.layerCount-1) {
 				var result = that.config.onSelectData(that.selected);
 				that.$element.textbox('setValue', result);
 				if (!that.config.multiple) {
@@ -270,19 +295,20 @@
 				}
 				return;
 			} else {
+				// Select a different item 
 				if (newOption) {
 					that.$element.textbox('clear');
 
-					// 关闭选中的layer之后的tabs
+					// Close tabs after layer+1
 					var allTabs = that.$tabs.tabs('tabs');
 					for (var i=allTabs.length-1; i>layer; i--) {
 						that.$tabs.tabs('close', i);
 					}
 					
-					// 加载下一层的数据
+					// Load next layer data
 					that.layerData[that.layers[layer+1].field] = that.layers[layer+1].dataProvider(data);
 					
-					// 增加一个tab
+					// Add a tab
 					that._addTab(layer+1);
 				} else {
 					that.$tabs.tabs('select', layer+1);
@@ -303,15 +329,14 @@
 		});
 	}
 
-	// 层级选择器默认参数
 	$.fn.layerselector.defaults = {
-		width: 300,
-		height: 200,
-		column: 4,
-		title: 'Search Layer',
-		layers: [],
-		multiple: false,
-		onSelectData: function(data) {
+		width: 300,					// The width of the panel
+		height: 200,				// The height of the panel
+		column: 4,					// The columns of items in panel
+		layers: [],					// Layers
+		multiple: false,			// Indicates multiple choices in the last layer
+		chracterPixels: 15,			// Pixels for a character
+		onSelectData: function(data) {		// Call when select in last layer
 			var $this = this, o = data[$this.layers[$this.layers.length-1].field];
 			if ($this.multiple) {
 				var arr = [];
@@ -322,17 +347,19 @@
 			} else {
 				return o[ $this.layers[$this.layers.length-1].labelField ];
 			}
-			
 		}
 	};
 
-	// 层级选择器某一层的默认参数
 	$.fn.layerselector.defaultLayer = {
-		dataProvider: function(selected) {},
-		idField: 'id',
-		labelField: 'label',
-		prompt: 'Select',
-		otherItem: function(selected) {return false;}
+		dataProvider: function(selected) {	// Provide this layer's data base on previous layer's selected
+
+		},		
+		idField: 'id',						// Id field name
+		labelField: 'label',				// Label field name
+		prompt: 'Select',					// Prompt message in the tab title initially
+		otherItem: function(selected) {		// Define whether other item should be display base on previous layer's selected
+			return false;
+		}
 	};
 
 })(jQuery);
